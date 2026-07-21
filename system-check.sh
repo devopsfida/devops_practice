f760acfcbd1ee131b32ed00e9e9daea35e7749b1
Fida Hussain <devopsfida@gmail.com>
2026-07-21 22:16:36 +0500

    feat: add initial automated system check script

---
+++ system-check.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Starting automated system check..."
+echo "Current user logged in: $USER"
+echo "Present working directory: $PWD"
+echo "System check complete. Environment is secure."
