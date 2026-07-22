9c2af842bf69d4f89272c0869f4b9bea053b66cf
Fida Hussain <devopsfida@gmail.com>
2026-07-22 13:05:05 +0500

    Files Updated

--- server-audit.sh
+++ server-audit.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 echo "---Server Audit Report---" > audit-log.txt
-
+echo "" >> audit-log.txt
 date >> audit-log.txt
-free -m >> audit-log.txt
+echo "" >> audit-log.txt
+#free -m >> audit-log.txt
+echo "" >> audit-log.txt
 df -h >> audit-log.txt
+echo "" >> audit-log.txt
